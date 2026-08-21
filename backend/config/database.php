01437e1da64b0779c3d65a46e0a3f9d851c7a72c
muhammadhafizh134 <2500018134@webmail.uad.ac.id>
2026-08-21 15:06:26 +0700

    Implementasi endpoint login dengan validasi dan token

--- backend/config/database.php
+++ backend/config/database.php
@@ -5,10 +5,8 @@ $username = 'root';
 $password = '';
 
 try {
-    $conn = new PDO("mysql:host = $host; dbname = $dbname; charset = utf8mb4", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e){
     echo "Koneksi gagal: " . $e->getMessage();
 }
-
-echo "Koneksi berhasil!";
